a029f455116436f0cbdb23b8624b230e5a280e1e
Drashti Ruparelia <155230292+DecentDrashti@users.noreply.github.com>
2024-03-26 22:06:55 +0530

    update Plus_one.java

--- My Leetcode_Codes/Java-Codes/Plus_one.java
+++ My Leetcode_Codes/Java-Codes/Plus_one.java
@@ -9,6 +9,26 @@ public class Day2_streak {
         System.out.println("Enter an integer:");
          digits[d] = sc.nextInt(); 
        }
+        //  int n=digits.length;
+       for(int i=(n-1);i>=0;i--){
+        if (digits[i]<9){
+            digits[i]++;
+            System.out.println(digits[i]);//return digits;
+        }
+        digits[i]=0;
+       }
+        // if array[i] is not less than 9, means it have to be 9 only then digit is changed to 0,
+	// and we again revolve around loop to check for number if less than 9 or not
+	// i.e. [ 5,9 ]-->[ 5,0 ]-loop->[ 6,0 ] or
+	//      [ 1,9,9 ]-->[ 1,9,0 ]-loop->[ 1,0,0 ]-loop->[ 2,0,0 ]
+	// and will directly return array
+       digits = new int[digits.length + 1];
+       digits[0] = 1;
+       for (int d = 0; d <n+1; d++) {//return digits;
+        System.out.println(digits[d]);
+       }
+    }
+}
 //        if(digits[n-1]<9){
 //         digits[n-1]+=1;
 //        }
